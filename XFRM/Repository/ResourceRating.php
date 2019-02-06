f8185af6d86912eabad2de05dc603318ae7c0757
Liam Williams <liamwli@users.noreply.github.com>
2019-02-06 13:27:01 +0000

    Move condition remover to own method (DRY)

--- XFRM/Repository/ResourceRating.php
+++ XFRM/Repository/ResourceRating.php
@@ -7,33 +7,20 @@ class ResourceRating extends XFCP_ResourceRating
 	public function findReviewsInResource(\XFRM\Entity\ResourceItem $resource, array $limits = [])
 	{
 		$finder = parent::findReviewsInResource($resource, $limits);
-
-		// Unfortunately, finders expose no method to remove a single where condition.
-		// We could completely override the method, but that might introduce incompatibilities,
-		// so instead have a low execution order, and re-add the existing conditions after resetting them.
-		$existingConditions = $finder->getConditions();
-		$finder->resetWhere();
-
-		foreach ($existingConditions AS $existingCondition)
-		{
-			$existingCondition = $this->stripReviewConditionFromStatement($existingCondition);
-
-			if (empty($existingCondition))
-			{
-				continue;
-			}
-
-			$finder->whereSql($existingCondition);
-		}
-
-		return $finder;
+		return $this->removeReviewConditionFromFinder($finder);
 	}
 
 	public function findLatestReviews(array $viewableCategoryIds = null)
 	{
 		$finder = parent::findLatestReviews($viewableCategoryIds);
+		return $this->removeReviewConditionFromFinder($finder);
+	}
 
-		// Same issue as above :(
+	protected function removeReviewConditionFromFinder(\XF\Mvc\Entity\Finder $finder)
+	{
+		// Unfortunately, finders expose no method to remove a single where condition.
+		// We could completely override the method, but that might introduce incompatibilities,
+		// so instead have a low execution order, and re-add the existing conditions after resetting them.
 		$existingConditions = $finder->getConditions();
 		$finder->resetWhere();
 
